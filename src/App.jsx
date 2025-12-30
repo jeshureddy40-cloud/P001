c0176351b3757a92820f87d21096d72544a11f61
jeshureddy40-cloud <jeshureddy40@gmail.com>
2025-12-30 12:53:31 +0530

    v1

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import'./App.css';
+function App (){
+    return(
+        <div className='app'>
+            <h1>Hello World !</h1>
+            <h2>Personal Profile</h2>
+            <h4>Name   : M.V Jeswanth Reddy</h4>
+            <h4>Program:ECE </h4>
+            <h4>Section:S-03</h4>
+            <h4>Course :Front End Devlopment Frameworks and UI Engineering</h4>
+        </div>
+    );
+}
+export default App;
