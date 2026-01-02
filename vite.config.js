76330b38b2ebcab9f95ee9caaf770507a987e1cd
jeshureddy40-cloud <jeshureddy40@gmail.com>
2026-01-02 10:26:29 +0530

    Fix vite config and install plugin

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/p001/'
-})
+  base: "/P001/", // for GitHub Pages
+});
